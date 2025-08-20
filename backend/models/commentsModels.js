325e5dc71ae52848b4ff2f7d1595ffc47a35775d
Lais Talita Brito de Oliveira <116667293+laisTalita@users.noreply.github.com>
2025-08-20 19:06:05 -0300

    Update commentsModels.js

--- backend/models/commentsModels.js
+++ backend/models/commentsModels.js
@@ -1,14 +1,20 @@
 const {Sequelize,DataTypes} =  require('sequelize')
 
-const sequelize =new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
-  {host:process.env.DB_HOST,dialect:process.env.DB_DIALECT,dialectOptions: {
+  {
+    host:process.env.DB_HOST,
+    dialect:process.env.DB_DIALECT,
+   dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
-      }});
+      }
+   }
+  }
+  );
 
 const Users = sequelize.define('users',{
   id:{
